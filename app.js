660b6427b51f837220b995702d1285d1a4f05ff8
Farheen <81709057+FarheenQadeer@users.noreply.github.com>
2021-04-03 15:26:29 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var a =12;
+var b =20;
+var c = 6;
+alert(a + ++a -b )
+
+var Farheen =16;
+var Sabeen=45;
+alert(Farheen) 
+
+
+var firstname ="Farheen" 
+var lastname ="Qadeer"
+alert(firstname+lastname)
+
+var x =prompt("How many siblings"); 
+if(x==="4"){
+    alert("true")
+}
+
+var weight =prompt("enter Ali's weight ")
+var Hight =prompt("enter Ali's Hight" )
+if(weight = 65 && Hight < 7){
+    alert("perfect guy" );
+}
+
+ else {
+alert ("weak" );}
